96a69e410c82337b4e28b8d12e5043c0228ea35a
jenseth <jenseth@Xubuntu.myguest.virtualbox.org>
2024-11-18 09:05:40 -0500

    changed output csv for redis

--- TP3-log8430-quebecTest/redis/script_redis2.sh
+++ TP3-log8430-quebecTest/redis/script_redis2.sh
@@ -5,38 +5,38 @@ printf "\nRunning Benchmarks on Redis DB, results can be found in the redis fold
 docker-compose -f docker-compose.yml up --scale redis-master=1 --scale redis-replica=4 -d
 
 # Initialisation des fichiers de résultats (écraser le contenu existant)
-echo "Initializing results for Load Redis Tests" > ../results/outputLoadRedis.csv
-echo "Initializing results for Run Redis Tests" > ../results/outputRunRedis.csv
+echo "Initializing results for Load Redis Tests" > ../results/outputLoadRedis2.csv
+echo "Initializing results for Run Redis Tests" > ../results/outputRunRedis2.csv
 
 cd ../YCSB
 for i in {1..10}
 do
   # Workload A
-  printf "\n##################################################################################\n" >> ../results/outputLoadRedis.csv
-  printf "Loading data workload A try $i\n" >> ../results/outputLoadRedis.csv
-  ./bin/ycsb load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputLoadRedis.csv
+  printf "\n##################################################################################\n" >> ../results/outputLoadRedis2.csv
+  printf "Loading data workload A try $i\n" >> ../results/outputLoadRedis2.csv
+  ./bin/ycsb load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputLoadRedis2.csv
 
-  printf "\n##################################################################################\n" >> ../redis/outputRunRedis.csv
-  printf "Running test workload A try $i\n" >> ../results/outputRunRedis.csv
-  ./bin/ycsb run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputRunRedis.csv
+  printf "\n##################################################################################\n" >> ../redis/outputRunRedis2.csv
+  printf "Running test workload A try $i\n" >> ../results/outputRunRedis2.csv
+  ./bin/ycsb run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputRunRedis2.csv
 
   # Workload B
-  printf "\n##################################################################################\n" >> ../results/outputLoadRedis.csv
-  printf "Loading data workload B try $i\n" >> ../results/outputLoadRedis.csv
-  ./bin/ycsb load redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputLoadRedis.csv
+  printf "\n##################################################################################\n" >> ../results/outputLoadRedis2.csv
+  printf "Loading data workload B try $i\n" >> ../results/outputLoadRedis2.csv
+  ./bin/ycsb load redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputLoadRedis2.csv
 
-  printf "\n##################################################################################\n" >> ../results/outputRunRedis.csv
-  printf "Running test workload B try $i\n" >> ../results/outputRunRedis.csv
-  ./bin/ycsb run redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputRunRedis.csv
+  printf "\n##################################################################################\n" >> ../results/outputRunRedis2.csv
+  printf "Running test workload B try $i\n" >> ../results/outputRunRedis2.csv
+  ./bin/ycsb run redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputRunRedis2.csv
 
   # Workload C
-  printf "\n##################################################################################\n" >> ../results/outputLoadRedis.csv
-  printf "Loading data workload C try $i\n" >> ../results/outputLoadRedis.csv
-  ./bin/ycsb load redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputLoadRedis.csv
+  printf "\n##################################################################################\n" >> ../results/outputLoadRedis2.csv
+  printf "Loading data workload C try $i\n" >> ../results/outputLoadRedis2.csv
+  ./bin/ycsb load redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputLoadRedis2.csv
 
-  printf "\n##################################################################################\n" >> ../results/outputRunRedis.csv
-  printf "Running test workload C try $i\n" >> ../results/outputRunRedis.csv
-  ./bin/ycsb run redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputRunRedis.csv
+  printf "\n##################################################################################\n" >> ../results/outputRunRedis2.csv
+  printf "Running test workload C try $i\n" >> ../results/outputRunRedis2.csv
+  ./bin/ycsb run redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.clustert=true" >> ../results/outputRunRedis2.csv
 done
 
 cd ../redis
